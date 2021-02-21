337b2396723641cf2fe687bfd3d7bc373dd49bd4
Jason Whitlock <jrwhitlock7@gmail.com>
2021-02-21 14:16:34 -0700

    commit tree 0a529f5579a91a91ad651d27da8be5e3e5ac9b92

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre, :turn_page
 
+    def initialize(title)
+        @title = title
+    end
+
+    def author=(author)
+        @author = author
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
